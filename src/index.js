7c116b1af0d704895eceb4704304c8183c509a3b
Eugeneche <53338476+Eugeneche@users.noreply.github.com>
2020-07-26 10:45:18 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const renderAll = (state) => {
 
     ReactDOM.render(
       <React.StrictMode>
-        <BrowserRouter basename="https://eugeneche.github.io/soc-net/">
+        <BrowserRouter basename="/soc-net">
           <Provider store={store}>
             <App />
           </Provider>
