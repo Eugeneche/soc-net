71c35e0a1a993dba21d1eb70ccd6f36c1b189696
Eugeneche <53338476+Eugeneche@users.noreply.github.com>
2020-08-03 11:45:56 +0200

    Updates

---
+++ precache-manifest.855688cf9c996f0d1f80a3ca4118b281.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee85bfb69251cccdede7bc9dd1a4623",
+    "url": "/soc-net/index.html"
+  },
+  {
+    "revision": "d5bf9c525dabee147b01",
+    "url": "/soc-net/static/css/main.ba940d7e.chunk.css"
+  },
+  {
+    "revision": "a9ed56073ca27f68c2f9",
+    "url": "/soc-net/static/js/2.a7532434.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/soc-net/static/js/2.a7532434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5bf9c525dabee147b01",
+    "url": "/soc-net/static/js/main.0ebef22e.chunk.js"
+  },
+  {
+    "revision": "997597f7e6f41d2fe53d",
+    "url": "/soc-net/static/js/runtime-main.635e479b.js"
+  },
+  {
+    "revision": "0c569380b7d34e5ba51c849ee5fff754",
+    "url": "/soc-net/static/media/looking_job.0c569380.svg"
+  },
+  {
+    "revision": "a380a2be3747875961450fe93654d19b",
+    "url": "/soc-net/static/media/main.a380a2be.jpg"
+  },
+  {
+    "revision": "d0c709f2f4e6a6410a284a71085480af",
+    "url": "/soc-net/static/media/not_looking_job.d0c709f2.svg"
+  },
+  {
+    "revision": "1ea9acb5347b256e3d64d2ba48378edc",
+    "url": "/soc-net/static/media/profile.1ea9acb5.jpg"
+  },
+  {
+    "revision": "8d17db1bbd4ffd44d7265f84182c78dd",
+    "url": "/soc-net/static/media/user_avatar_placeholder.8d17db1b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/soc-net/precache-manifest.53752b344ceaf40f22d61c9d50431b85.js"
+  "/soc-net/precache-manifest.855688cf9c996f0d1f80a3ca4118b281.js"
 );
 
 self.addEventListener('message', (event) => {
